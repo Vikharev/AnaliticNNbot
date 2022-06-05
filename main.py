33400a159d61aeba01f07bdd7f113e83634c1c28
Вихарев Алексей <a.v.vikharev@gmail.com>
2022-06-05 14:04:01 +0300

    commit

---
+++ main.py
@@ -0,0 +1,82 @@
+import fileinput
+import os.path
+import telebot
+from telebot import types
+import re
+import requests
+from dotenv import load_dotenv
+
+from vk_functions import get_big_list
+
+load_dotenv()
+
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+
+bot = telebot.TeleBot(token=TELEGRAM_TOKEN)
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    if message.from_user.first_name is not None:
+        from_user_first_name = message.from_user.first_name
+    else:
+        from_user_first_name = ''
+    if message.from_user.last_name is not None:
+        from_user_last_name = message.from_user.last_name
+    else:
+        from_user_last_name = ''
+    mess = f'Привет, <b>{from_user_first_name} {from_user_last_name} {message.from_user}</b>!\n Чем помочь?'
+    bot.send_message(message.chat.id, mess, parse_mode='html')
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+    btn1 = types.KeyboardButton('Анализ странички ВКонтакте')
+    btn2 = types.KeyboardButton('Пока еще посто бесполезная кнопка')
+    markup.add(btn1, btn2)
+    bot.send_message(message.chat.id, 'Пока я еще только учусь, так что не судите строго', reply_markup=markup)
+
+
+@bot.message_handler(commands=['vk'])
+def url_vk(message):
+    markup = types.InlineKeyboardMarkup()
+    markup.add(types.InlineKeyboardButton('Перейти на vk.com', url='https://vk.com'))
+    bot.send_message(message.chat.id, 'Ссылка', reply_markup=markup)
+
+
+@bot.message_handler(content_types=['text'])
+def funcs(message):
+    print(message.chat.id)
+    print(message.forward_from)
+    print(message.forward_from_chat)
+    token = '5396046759:AAFD6plKcs_G2_Y_mCn0IkVj5DTkuzunMRE'
+    data = {'chat_id': 613544738, 'user_id': message.from_user.id}
+    r = requests.post(f'https://api.telegram.org/bot{token}/getChatMember', data=data)
+    print(r.text)
+    print(r.json())
+    print(message.chat)
+    if re.fullmatch(r'\d*', message.text):
+        if os.path.exists(f'reports/{message.text}.txt'):
+            bot.send_message(message.chat.id, 'По этому профилю отчет уже есть. Держи.', parse_mode='html')
+            f = open(f'reports/{message.text}.txt', 'rb')
+            bot.send_document(message.chat.id, f)
+        else:
+            temp_message_report = bot.send_message(message.chat.id, 'Отчет формируется. Немного подождите.', parse_mode='html')
+            with open(f'reports/{message.text}.txt', 'w+', encoding='utf-8') as f:
+                info2 = get_big_list(message.text)
+                f.write("Расширенный список друзей пользователя\n" + info2)
+            f = open(f'reports/{message.text}.txt', 'rb')
+            bot.delete_message(chat_id=message.chat.id, message_id=temp_message_report.id)
+            bot.send_document(message.chat.id, f)
+
+    elif message.text == 'Анализ странички ВКонтакте':
+        bot.send_message(message.chat.id, 'Введи id пользователя\n<u>(только числовое значение)</u>', parse_mode='html')
+    elif message.text == 'Hello':
+        bot.send_message(message.chat.id, 'Hi!', parse_mode='html')
+    elif message.text == 'id':
+        bot.send_message(message.chat.id, f'Твой ID: {message.from_user.id}', parse_mode='html')
+    elif message.text == 'photo':
+        photo = open('AnaliticNNbot.jpg', 'rb')
+        bot.send_photo(message.chat.id, photo)
+    else:
+        bot.send_message(message.chat.id, message.text, parse_mode='html')
+
+
+bot.polling(none_stop=True)
---
+++ vk_functions.py
@@ -0,0 +1,343 @@
+from urllib.request import urlopen
+import json
+import time
+import requests
+import re
+from bs4 import BeautifulSoup
+
+
+def get_big_list(user_id):
+    urlFull = 'https://api.vk.com/method/friends.get?user_id=' + user_id + '&lang=ru&fields=schools,status,last_seen,occupation,nickname,relatives,relation,personal,connections,exports,activities,interests,music,movies,tv,books,games,about,quotes,career,nickname,domain,bdate,city,country,photo_100,has_mobile,contacts,education,relation,last_seen,universities,status&access_token=453fde99453fde99453fde99bf454896bf4453f453fde9925511e1d9ac8c5dd3cc4f27e&v=5.130'
+    respFull = urlopen(urlFull)
+    htmlFull = respFull.read()
+    reqVkFull = json.loads(htmlFull)
+    try:
+        answerFull = reqVkFull['response']['items']
+    except:
+        info5 = 'Профиль скрыт настройками приватности. Данные носят ориентировочный характер.\n\n'
+        info5 += get_big_list_private(user_id)
+        return info5
+    info2 = ''
+    for friend in answerFull:
+        time.sleep(0.1)
+        info2 += 'id'
+        info2 += str(friend['id'])
+        friendID = 'id' + str(friend['id'])
+        try:
+            domain = friend['domain']
+            if domain != '' and domain != friendID:
+                info2 += ' ('
+                info2 += domain
+                info2 += ')'
+        except:
+            nickname = ''
+        info2 += ' - '
+        info2 += friend['first_name']
+        try:
+            nickname = friend['nickname']
+            if nickname != '':
+                info2 += ' '
+                info2 += nickname
+        except:
+            nickname = ''
+        info2 += ' '
+        info2 += friend['last_name']
+        try:
+            bdate = friend['bdate']
+            if bdate != '':
+                info2 += ', ДР: '
+                info2 += bdate
+        except:
+            bdate = ''
+        try:
+            country = friend['country']['title']
+            if country != '':
+                info2 += ', '
+                info2 += country
+        except:
+            country = ''
+        try:
+            city = friend['city']['title']
+            if city != '':
+                info2 += ', '
+                info2 += city
+        except:
+            city = ''
+        try:
+            university_name = friend['university_name']
+            if university_name != '' and university_name != '0':
+                info2 += ', '
+                info2 += university_name
+        except:
+            university_name = ''
+        try:
+            faculty_name = friend['faculty_name']
+            if faculty_name != '' and faculty_name != '0':
+                info2 += ', '
+                info2 += faculty_name
+        except:
+            faculty_name = ''
+        try:
+            graduation = friend['graduation']
+            if graduation != '' and graduation != '0' and len(graduation) > 1:
+                info2 += ' ('
+                info2 += graduation
+                info2 += ')'
+        except:
+            graduation = ''
+        try:
+            occupation = friend['occupation']['name']
+            if occupation != '' and occupation != '0' and occupation != university_name:
+                info2 += ', '
+                info2 += occupation
+        except:
+            occupation = ''
+        try:
+            career = friend['career']
+            if len(career) > 0:
+                for work in career:
+                    if work['company'] != occupation:
+                        info2 += ', '
+                        info2 += work['company']
+                    try:
+                        position = work['position']
+                        if len(position) > 1:
+                            info2 += ' '
+                            info2 += position
+                    except:
+                        position = ''
+                    try:
+                        workfrom = work['from']
+                        if len(str(workfrom)) > 1:
+                            info2 += ' c '
+                            info2 += workfrom
+                    except:
+                        workfrom = ''
+                    try:
+                        workuntil = work['until']
+                        if len(str(workuntil)) > 1:
+                            info2 += ' по '
+                            info2 += workuntil
+                    except:
+                        workuntil = ''
+        except:
+            career = ''
+        try:
+            relation = friend['relation']
+            if relation == 1:
+                info2 += ', не женат/не замужем'
+            elif relation == 4:
+                info2 += ', женат/замужем'
+            elif relation == 7:
+                info2 += ', влюблен/влюблена'
+        except:
+            relation = ''
+        try:
+            relation_partner = friend['relation_partner']
+            if len(relation_partner) > 0:
+                info2 += ' ('
+                info2 += friend['relation_partner']['first_name']
+                info2 += ' '
+                info2 += friend['relation_partner']['last_name']
+                info2 += ', id'
+                info2 += friend['relation_partner']['id']
+                info2 += ')'
+        except:
+            relation_partner = ''
+        try:
+            mobile_phone = friend['mobile_phone']
+            if mobile_phone != '' and mobile_phone != '0' and len(mobile_phone) > 4:
+                info2 += ', '
+                info2 += mobile_phone
+        except:
+            mobile_phone = ''
+        try:
+            home_phone = friend['home_phone']
+            if home_phone != '' and home_phone != '0' and len(home_phone) > 4:
+                info2 += ', '
+                info2 += home_phone
+        except:
+            home_phone = ''
+        info2 += '\n'
+    return info2
+
+
+def get_big_list_private(user_id):
+    info5 = ''
+    url = "https://onli-vk.ru/pivatfriends.php?id=" + user_id
+    header = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.141 YaBrowser/22.3.3.852 Yowser/2.5 Safari/537.36',
+        'cookie': '_ym_uid=16525161441053431871; _ym_d=1652516144; _ym_isad=2; _ym_visorc=b'
+    }
+    req = requests.get(url, headers=header)
+    soup = BeautifulSoup(req.text, 'html.parser')
+    friends = {}
+    for a in soup.find_all('a'):
+        if 'vk.com/id' in a['href']:
+            friendID = str(a)
+            pos = friendID.find('com/id')
+            friendID = friendID[pos + 6:]
+            pos = friendID.find('\"')
+            friendID = friendID[:pos]
+            name = str(a)
+            pos = name.find('>')
+            name = name[pos + 1:]
+            pos = name.find('<')
+            name = name[:pos]
+            if friendID != user_id:
+                friends[friendID] = name
+    user_id = ''
+    for i in friends:
+        user_id = user_id + i
+        user_id = user_id + ','
+    user_id = user_id[0:-1]
+    urlFull = 'https://api.vk.com/method/users.get?user_ids=' + user_id + '&lang=ru&fields=schools,status,last_seen,occupation,nickname,relatives,relation,personal,connections,exports,activities,interests,music,movies,tv,books,games,about,quotes,career,nickname,domain,bdate,city,country,photo_100,has_mobile,contacts,education,relation,last_seen,universities,status&access_token=453fde99453fde99453fde99bf454896bf4453f453fde9925511e1d9ac8c5dd3cc4f27e&v=5.130'
+    respFull = urlopen(urlFull)
+    htmlFull = respFull.read()
+    reqVkFull = json.loads(htmlFull)
+    try:
+        answerFull = reqVkFull['response']
+    except:
+        answerFull = 'Закрытый профиль'
+    for friend in answerFull:
+        time.sleep(0.2)
+        info5 += 'id'
+        info5 += str(friend['id'])
+        friendID = 'id' + str(friend['id'])
+        try:
+            domain = friend['domain']
+            if domain != '' and domain != friendID:
+                info5 += ' ('
+                info5 += domain
+                info5 += ')'
+        except:
+            nickname = ''
+        info5 += ' - '
+        info5 += friend['first_name']
+        try:
+            nickname = friend['nickname']
+            if nickname != '':
+                info5 += ' '
+                info5 += nickname
+        except:
+            nickname = ''
+        info5 += ' '
+        info5 += friend['last_name']
+        try:
+            bdate = friend['bdate']
+            if bdate != '':
+                info5 += ', ДР: '
+                info5 += bdate
+        except:
+            bdate = ''
+        try:
+            country = friend['country']['title']
+            if country != '':
+                info5 += ', '
+                info5 += country
+        except:
+            country = ''
+        try:
+            city = friend['city']['title']
+            if city != '':
+                info5 += ', '
+                info5 += city
+        except:
+            city = ''
+        try:
+            university_name = friend['university_name']
+            if university_name != '' and university_name != '0':
+                info5 += ', '
+                info5 += university_name
+        except:
+            university_name = ''
+        try:
+            faculty_name = friend['faculty_name']
+            if faculty_name != '' and faculty_name != '0':
+                info5 += ', '
+                info5 += faculty_name
+        except:
+            faculty_name = ''
+        try:
+            graduation = friend['graduation']
+            if graduation != '' and graduation != '0' and len(graduation) > 1:
+                info5 += ' ('
+                info5 += graduation
+                info5 += ')'
+        except:
+            graduation = ''
+        try:
+            occupation = friend['occupation']['name']
+            if occupation != '' and occupation != '0' and occupation != university_name:
+                info5 += ', '
+                info5 += occupation
+        except:
+            occupation = ''
+        try:
+            career = friend['career']
+            if len(career) > 0:
+                for work in career:
+                    if work['company'] != occupation:
+                        info5 += ', '
+                        info5 += work['company']
+                    try:
+                        position = work['position']
+                        if len(position) > 1:
+                            info5 += ' '
+                            info5 += position
+                    except:
+                        position = ''
+                    try:
+                        workfrom = work['from']
+                        if len(str(workfrom)) > 1:
+                            info5 += ' c '
+                            info5 += workfrom
+                    except:
+                        workfrom = ''
+                    try:
+                        workuntil = work['until']
+                        if len(str(workuntil)) > 1:
+                            info5 += ' по '
+                            info5 += workuntil
+                    except:
+                        workuntil = ''
+        except:
+            career = ''
+        try:
+            relation = friend['relation']
+            if relation == 1:
+                info5 += ', не женат/не замужем'
+            elif relation == 4:
+                info5 += ', женат/замужем'
+            elif relation == 7:
+                info5 += ', влюблен/влюблена'
+        except:
+            relation = ''
+        try:
+            relation_partner = friend['relation_partner']
+            if len(relation_partner) > 0:
+                info5 += ' ('
+                info5 += friend['relation_partner']['first_name']
+                info5 += ' '
+                info5 += friend['relation_partner']['last_name']
+                info5 += ', id'
+                info5 += friend['relation_partner']['id']
+                info5 += ')'
+        except:
+            relation_partner = ''
+        try:
+            mobile_phone = friend['mobile_phone']
+            if mobile_phone != '' and mobile_phone != '0' and len(mobile_phone) > 4:
+                info5 += ', '
+                info5 += mobile_phone
+        except:
+            mobile_phone = ''
+        try:
+            home_phone = friend['home_phone']
+            if home_phone != '' and home_phone != '0' and len(home_phone) > 4:
+                info5 += ', '
+                info5 += home_phone
+        except:
+            home_phone = ''
+        info5 += '\n'
+    return info5
