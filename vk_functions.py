ba9cab3944cc20d8cedbaabd0c5cefb539b6aa32
Вихарев Алексей <a.v.vikharev@gmail.com>
2022-06-18 10:28:26 +0300

    commit

--- vk_functions.py
+++ vk_functions.py
@@ -41,6 +41,7 @@ def get_list_friends(list_ids: list) -> list:
                     friends.append(friendID)
         list_friends.extend(friends)
     c = Counter(list_friends)
+    print(c)
     clear_list = [x for x in list_friends if c[x] >= len(list_friends)-1]
     print(clear_list)
     return clear_list
